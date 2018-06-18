b56e2327c704bbe94d5bf5508c539587ac754430
cmaniscalchi <claudia.maniscalchi@gmail.com>
2018-06-18 18:32:23 -0400

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,58 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_number, season_hash|
+    if season_number == season
+      season_hash.each do |contestant|
+        if contestant["status"] == "Winner"
+          return contestant["name"].split[0]
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_number, season_hash|
+    season_hash.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+counter = 0
+  data.each do |season_number, season_hash|
+    season_hash.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_number, season_hash|
+    season_hash.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  age_array = []
+  data.each do |season_number, season_hash|
+    if season_number == season
+      season_hash.each do |contestant|
+        age_array << contestant["age"].to_i
+      end
+    end
+  end
+  avg = age_array.reduce(:+).to_f/age_array.length.to_f
+  avg.round
 end
