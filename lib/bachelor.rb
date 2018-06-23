584be98862b4c11c72407fb8526a2de8150aefd8
Daniel Ran <danielr1735@gmail.com>
2018-06-23 04:42:44 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,3 +1,4 @@
+
 require_relative "../lib/bachelor.rb"
 require 'json'
 
@@ -39,26 +40,59 @@ data = JSON.parse(File.read('spec/fixtures/contestants.json'))
 #   ]
 # }
 def get_first_name_of_season_winner(data, season)
-
-  puts winner = data[season].find { |x| x["status"] == "Winner" }
-  winner.fetch("name").split(" ")[0]
-
+  data[season].find{ |x| x["status"] == "Winner" }.fetch("name").split(" ")[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  found = nil
+  while !found
+    data.each do |season, list|
+      current = list.find {|contestant| contestant.fetch("occupation") == occupation }
+      if current
+        found = current.fetch("name")
+      end
+    end
+  end
+found
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |season, list|
+    list.each do |contestant|
+      if contestant.fetch("hometown") == hometown
+        counter += 1
+      end
+    end
+  end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  found = nil
+  while !found
+    data.each do |season, list|
+      current = list.find {|contestant| contestant.fetch("hometown") == hometown }
+      if current
+        found = current.fetch("occupation")
+      end
+    end
+  end
+found
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  total = 0
+  counter = 0
+  data[season].each do |contestant|
+    total += contestant.fetch("age").to_i
+    counter += 1
+  end
+  (total/counter).ceil
 end
 
-get_first_name_of_season_winner(data, "season 18")
+# get_first_name_of_season_winner(data, "season 18")
+
+# get_contestant_name(data, "Chiropractic Assistant")
+
+get_average_age_for_season(data, "season 18")
